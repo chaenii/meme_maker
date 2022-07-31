c3b0ba3c79515c0757d8bb17e86547dd080fba3b
Chaenii <83631140+Chaenii@users.noreply.github.com>
2022-07-31 16:03:06 +0900

    use color brush with mouse

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+const colorOptions = Array.from(document.getElementsByClassName("color-option"));
+const color = document.getElementById("color");
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 
 const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
 function onMove(event) {
@@ -22,9 +25,31 @@ function startPainting() {
 
 function canclePainting() {
   isPainting = false;
+  ctx.beginPath();
+}
+
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", canclePainting);
 canvas.addEventListener("mouseleave", canclePainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("chage", onColorChange);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
